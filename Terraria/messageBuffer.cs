bc5b4f7064a7c3b5be114fdac5edc3644f40ca63
Simon <mkontent@gmail.com>
2014-05-13 01:50:51 +0400

    Remove Close()

--- Terraria/messageBuffer.cs
+++ Terraria/messageBuffer.cs
@@ -33,8 +33,6 @@ namespace Terraria
 			this.readBuffer = new byte[65535];
 			this.writeBuffer = new byte[65535];
 
-			binaryReader.Close();
-			binaryWriter.Close();
 			binaryReader.Dispose();
 			binaryWriter.Dispose();
 			binaryReader = new BinaryReader(new MemoryStream(readBuffer));
