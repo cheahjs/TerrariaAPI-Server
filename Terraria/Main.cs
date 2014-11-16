1df841debe11d1f913e8f790e257cecef76bbe46
Jim Duhala <jim.duhala@gmail.com>
2014-11-16 14:17:36 -0500

    Re-added butts.
    Make it end.

--- Terraria/Main.cs
+++ Terraria/Main.cs
@@ -12,7 +12,7 @@ namespace Terraria
 {
 	public class Main
 	{
-
+        //Butts.
 		public static int maxItemTypes = 2750;      // GitFlip - Max Items
 		public static int maxProjectileTypes = 424; // GitFlip - Max Projectiles
 		public static int maxNPCTypes = 378;
